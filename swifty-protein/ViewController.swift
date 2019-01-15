1ee957e14d370676b4b853f935c79885980932e6
Kristine Sonu <moonyoning@Kristines-MacBook-Pro.local>
2019-01-15 15:31:33 -0800

    appicon and launchscreen

---
+++ swifty-protein/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swifty-protein
+//
+//  Created by Kristine Sonu on 1/15/19.
+//  Copyright © 2019 Kristine Sonu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
